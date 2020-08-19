1380e990fe820c52116111671d5cfa58b835091a
Dan Staples <dan@disman.tl>
2020-08-18 20:29:48 -0400

    Several fixes and updates for bot

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,17 @@
 import os
 import sys
+import copy
+import logging
 from datetime import datetime
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'BPDWatch'))
 from OpenOversight.app.models import Officer
+
+loglevel = os.environ.get('LOGLEVEL', 'INFO').upper()
+numeric_level = getattr(logging, loglevel, None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % loglevel)
+logger = logging.getLogger(__file__)
+logger.setLevel(numeric_level)
 
 equivalent_suffixes = [
     ('sr'),
@@ -29,6 +38,7 @@ class OfficerMatcher:
     
     @classmethod
     def permutate_names(cls, officer):
+        logger.debug(f'Permutating name for {officer.full_name()}')
         names = [f'{officer.first_name} {officer.last_name}']
         if officer.middle_initial:
             if len(officer.middle_initial) > 1:
@@ -37,21 +47,25 @@ class OfficerMatcher:
         if officer.suffix:
             for suffix_set in equivalent_suffixes:
                 if officer.suffix.lower() in suffix_set:
+                    names_copy = copy.deepcopy(names)
                     for suffix in suffix_set:
-                        for name in names:
+                        for name in names_copy:
                             names.append(f'{name} {suffix}')
         return names
 
     def load_officers(self):
+        logger.info('Loading officers')
         self.officer_names = self.fetch_officers()
         self.last_updated = datetime.now()
 
     def match_officers(self, text):
-        if (self.last_updated - datetime.now()).total_seconds > self.update_seconds:
+        logger.debug(f'Matching against {text}')
+        if (self.last_updated - datetime.now()).total_seconds() > self.update_seconds:
             self.load_officers()
         matched_officers = []
         for officer_set in self.officer_names:
             for name in officer_set['names']:
-                if name in text:
+                if name.lower() in text.lower():
+                    logging.info(f"Matched officer {officer_set['officer'].full_name()}")
                     matched_officers.append(officer_set['officer'])
         return matched_officers
--- handlers/bpd.py
+++ handlers/bpd.py
@@ -1,61 +1,69 @@
 import os
 import sys
+import locale
+import logging
 import requests
 from time import sleep
 from . import OfficerMatcher
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'ResponseBot'))
 from responsebot.handlers import BaseTweetHandler, register_handler
+from responsebot.models import TweetFilter
+
+locale.setlocale(locale.LC_ALL, '')
 
 
 def generate_tweet(officer):
     text = ""
     if officer.assignments[0].job.job_title != 'Not Sure':
         text += f"{officer.assignments[0].job.job_title} "
-    text += f"""
-    {officer.full_name()} ({officer.unique_internal_identifier.upper()}) made 
-    ${officer.salaries[0].salary - officer.salaries[0].overtime_pay} in {officer.salaries[0].year}.
-    """
+    total_pay = officer.salaries[0].salary - officer.salaries[0].overtime_pay
+    text += f"{officer.full_name()} ({officer.unique_internal_identifier.upper()}) made "
+    text += f"{locale.currency(total_pay, grouping=True)} in {officer.salaries[0].year}."
     if officer.incidents:
         text += f" {officer.last_name} was involved in {len(officer.incidents)} incidents."
     text += f" Full profile: https://bpdwatch.com/officer/{officer.id}."
-    print(f"Generated tweet: {text}")
+    logging.info(f"Generated tweet: {text}")
     return text
 
 
 @register_handler
 class BPDHandler(BaseTweetHandler):
-    def __init__(self):
+    def __init__(self, client):
+        super().__init__(client)
         self.matcher = OfficerMatcher()
 
+    def get_filter(self):
+     return TweetFilter(track=['@whoisbpd'])
+
     def on_tweet(self, tweet):
-        matched_officers = []
-        
         def parse_tweet(_tweet):
-            print(f"Parsing tweet: {_tweet.text}")
+            logging.info(f"Parsing tweet: {_tweet.text}")
             # remove periods, lower case, remove double+ spaces bw words
             tweet_text = ' '.join(_tweet.text.lower().replace('.','').split())
-            matched_officers += self.matcher.match_officers(tweet_text)
+            officers = self.matcher.match_officers(tweet_text)
             # Check text of any links
             for link in _tweet.entities['urls']:
                 r = requests.get(link['expanded_url'])
                 if r.status_code == 200:
-                    matched_officers += self.matcher.match_officers(r.text)
+                    officers += self.matcher.match_officers(r.text)
+            return officers
 
-        parse_tweet(tweet)
+        matched_officers = parse_tweet(tweet)
         # Check text of quote retweet
         if tweet.is_quote_status:
-            parse_tweet(tweet.quoted_status)
+            matched_officers += parse_tweet(tweet.quoted_status)
 
         for officer in matched_officers:
+            tweet_text = f'@{tweet.user.screen_name} ' + generate_tweet(officer)
             self.client.tweet(
-                generate_tweet(officer),
+                tweet_text,
                 in_reply_to=tweet.id
             )
             sleep(1)
     
     def on_direct_message(self, message):
         msg_text = message.message_create['message_data']['text']
-        print(f"Parsing direct message: {msg_text}")
+        logging.info(f"Parsing direct message: {msg_text}")
         
         # remove periods, lower case, remove double+ spaces bw words
         msg_text = ' '.join(msg_text.lower().replace('.','').split())
